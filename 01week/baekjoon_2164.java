62fb6b147986c7cd56a4412bfab0a7c5412e2437
hoohoha <jae7729@gmail.com>
2024-07-02 17:44:18 +0900

    Update baekjoon_2164.java

--- 01week/baekjoon_2164.java
+++ 01week/baekjoon_2164.java
@@ -5,9 +5,10 @@ public class Main{
         int a=sc.nextInt();
         int n=0;
         Queue <Integer> q= new LinkedList<Integer>();
-
+        // 큐에 순서대로 추가
         for(int i=1;i<a+1;i++)
             q.add(i);
+        // 제일 위에 있는 거 버리고, 다음 거 맨밑으로 옮기기
         for(int j=a;j>1;j--)
         {
             q.poll();
