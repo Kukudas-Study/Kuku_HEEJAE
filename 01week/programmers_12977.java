ea3537f97f92db18492d552d3c32f72239eef6f6
hoohoha <jae7729@gmail.com>
2024-07-02 16:57:37 +0900

    Update programmers_12977.java

--- 01week/programmers_12977.java
+++ 01week/programmers_12977.java
@@ -1,8 +1,9 @@
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-
+        // 소수 판별 -> 소수 : true , 소수 X : false
         boolean b=true;
+        // 숫자 3개 선택
         for(int i=0; i<nums.length-2 ;i++)
         {
             for (int j=i+1;j<nums.length-1;j++)
@@ -10,6 +11,7 @@ class Solution {
                 for(int p=j+1; p<nums.length; p++)
                 {
                     int sum=nums[i]+nums[j]+nums[p];
+                    // 소수 판별
                     for(int k=2; k<sum ; k++)
                     {
                         b=true;
@@ -19,6 +21,7 @@ class Solution {
                             break;
                         }
                     }
+                    // 소수일때
                     if(b)
                         answer++;
                 }
