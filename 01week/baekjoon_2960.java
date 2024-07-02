0fc09ef7fc04a38ee1445f94bcb4d6a0dd6bc13b
hoohoha <jae7729@gmail.com>
2024-07-02 17:28:34 +0900

    Update baekjoon_2960.java

--- 01week/baekjoon_2960.java
+++ 01week/baekjoon_2960.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[]args){
+        //입력 받은 수
         Scanner sc= new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
         int cnt=0;
         boolean[] list=new boolean[a+1];
+        // 자신의 배수 지우기 -> 지우면 true
         for(int i=2; i<=a;i++)
         {
             if(list[i])
