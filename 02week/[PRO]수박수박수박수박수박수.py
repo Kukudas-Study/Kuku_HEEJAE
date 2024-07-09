80eee3a00aea87d740ecace9f37b859f9fe8d9ad
hoohoha <jae7729@gmail.com>
2024-07-09 10:01:25 +0900

    solve : [PRO] 수박수박수박수박수박수_240708

--- 02week/[PRO]수박수박수박수박수박수.py
+++ 02week/[PRO]수박수박수박수박수박수.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = ''
-	  # 수박을 반복하는 문자열
+    # 수박을 반복하는 문자열
     str='수박'*n
-	  # 반복되는 수박을 입력받은 n번째 전까지 추출
+    # 반복되는 수박을 입력받은 n번째 전까지(0부터 시작) 추출
     answer=str[:n]
     return answer
