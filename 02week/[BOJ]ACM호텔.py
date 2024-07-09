195d2d410618728baf6a4aeb779f61204f84741e
hoohoha <jae7729@gmail.com>
2024-07-09 11:09:52 +0900

    solve : [BOJ] ACM호텔_240708

--- 02week/[BOJ]ACM호텔.py
+++ 02week/[BOJ]ACM호텔.py
@@ -1,3 +1,4 @@
+# 1호수부터 배정됨 => 1층 1호, 2층 1호 .. 순으로 
 T = int(input())
 # h : 호텔 층 수 , w : 각 층의 방 수, n : 몇 번째 손님
 for i in range(T):
@@ -6,6 +7,8 @@ for i in range(T):
     floor_num = n % h 
     floor_room_num = (n // h) + 1
 
+    # n 번째 손님이 호텔 층 수의 배수일 경우
+    # 무조건 호텔 층 수인 층을 사용
     if floor_num == 0:
         floor_num = h
         floor_room_num -=1
