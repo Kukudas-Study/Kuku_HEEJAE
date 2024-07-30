0c12b130ff939a3a7e9fe983656693bede341b54
hoohoha <jae7729@gmail.com>
2024-07-30 10:44:29 +0900

    solve : [BOJ] N과 M (3)_240730

---
+++ 05week/[BOJ]N과M(3).py
@@ -0,0 +1,17 @@
+n,m= map(int,input().split())
+arr = []
+ 
+def back():
+    # 길이가 m일 때 출력
+    # 문자열로 변환하고, 공백으로 구분해 출력
+    if len(arr)==m:
+        print(' '.join(map(str,arr)))
+        return
+   
+    # 1~n까지 순회하며 리스트에 추가하고, back()호출, 리스트 비우기
+    for i in range(1,n+1):
+        arr.append(i)
+        back()
+        arr.pop()
+# 백트래킹 시작        
+back()
